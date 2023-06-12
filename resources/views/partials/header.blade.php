738b91ac2a0f1fc12de43ecbfb5baa870ece5dd1
Devfrank94 <francescomurro94@gmail.com>
2023-06-12 16:10:36 +0200

    add file config and logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
-<header class="p-3">
-
+<header class="p-3 text-center">
+    <img class="my-4" src="/dc-logo.png" alt="DC Logo">
     <ul class="nav nav-tabs">
         <li class="nav-item">
           <a class="nav-link {{ Route::currentRouteName() === 'home' ? 'active' : '' }} " href="{{ route('home') }}">Home</a>
